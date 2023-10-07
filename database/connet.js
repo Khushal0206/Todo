238fae2af901c2bfaa4c6f6400e3512076e634d5
Khushal0206 <vermakhushaal@gmail.com>
2023-10-07 20:24:10 +0530

    fixed engine

--- database/connet.js
+++ database/connet.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose")
  const database =()=>{
-     mongoose.connect(process.env.MONGO_URL).then(()=>{console.log("database conneted ")}).catch(()=>{console.log("error")})
+     mongoose.connect(process.env.MONGO_URL,{
+        dbName: "Todo",
+      }).then(()=>{console.log("database conneted ")}).catch(()=>{console.log("error")})
 }
 module.exports = database
